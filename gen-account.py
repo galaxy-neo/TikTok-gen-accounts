f8dbabb974471c68f4962af6421bcadeeb0da7e4
neo galaxy <gneo0194@gmail.com>
2025-08-30 16:14:44 +0300

    Update gen-account.py

--- gen-account.py
+++ gen-account.py
@@ -2,7 +2,6 @@
 **Contact for api :** [https://t.me/Neo_Galaxy](https://t.me/Neo_Galaxy)
 ## telegram: @Neo_Galaxy
 
-  
 ### contact me
 **Contact for api :** [https://t.me/Neo_Galaxy](https://t.me/Neo_Galaxy)
 ## telegram: @Neo_Galaxy
